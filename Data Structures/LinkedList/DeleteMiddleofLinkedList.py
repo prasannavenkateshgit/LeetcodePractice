a3d35ebebc55f68990f8d96d611d26405a435c42
prasannavenkateshgit <prasannav710@gmail.com>
2024-12-13 17:37:44 -0800

    Update DeleteMiddleofLinkedList.py

--- Data Structures/LinkedList/DeleteMiddleofLinkedList.py
+++ Data Structures/LinkedList/DeleteMiddleofLinkedList.py
@@ -33,3 +33,21 @@ class Solution:
             start=start.next
             i+=1
         #return head
+
+#BETTER SOLUTION
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow=fast=head
+        dummy=ListNode(0,head)
+        prev=dummy
+        while fast and fast.next:
+            prev=slow
+            slow=slow.next
+            fast=fast.next.next
+        prev.next=prev.next.next
+        return dummy.next
