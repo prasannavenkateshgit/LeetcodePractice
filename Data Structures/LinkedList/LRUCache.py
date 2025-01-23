8992cec212d5231104d0b68cf1b7e09e744a2985
prasannavenkateshgit <prasannav710@gmail.com>
2025-01-22 21:46:09 -0800

    Create LRUCache.py

---
+++ Data Structures/LinkedList/LRUCache.py
@@ -0,0 +1,64 @@
+'''
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+'''
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.left = Node(0,0)
+        self.right = Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+        
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def ins(self, node):
+        prev = self.right.prev
+        prev.next = node
+        self.right.prev = node
+        node.prev = prev
+        node.next = self.right
+    
+    
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.ins(self.cache[key])
+            return self.cache[key].val
+        else:
+            return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.ins(self.cache[key])
+        if len(self.cache)>self.capacity:
+            temp = self.left.next
+            self.remove(temp)
+            del self.cache[temp.key]
+
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
