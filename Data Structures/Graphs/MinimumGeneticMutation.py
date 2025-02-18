4812a27e84c339d3a1ee9f096ec6801b435a92bf
prasannavenkateshgit <prasannav710@gmail.com>
2025-02-18 11:25:15 -0800

    Create MinimumGeneticMutation.py

---
+++ Data Structures/Graphs/MinimumGeneticMutation.py
@@ -0,0 +1,41 @@
+'''
+A gene string can be represented by an 8-character long string, with choices from 'A', 'C', 'G', and 'T'.
+
+Suppose we need to investigate a mutation from a gene string startGene to a gene string endGene where one mutation is defined as one single character changed in the gene string.
+
+For example, "AACCGGTT" --> "AACCGGTA" is one mutation.
+There is also a gene bank bank that records all the valid gene mutations. A gene must be in bank to make it a valid gene string.
+
+Given the two gene strings startGene and endGene and the gene bank bank, return the minimum number of mutations needed to mutate from startGene to endGene. If there is no such a mutation, return -1.
+
+Note that the starting point is assumed to be valid, so it might not be included in the bank.
+'''
+class Solution:
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        def neighbors(s):
+            ans=[]
+            copy=[]
+            chars=["A","C","G","T"]
+            for i in range(len(s)):
+                for j in chars:
+                    temp=s[:i]+j+s[i+1:]
+                    if temp in bank:
+                        ans.append(temp)
+            return ans
+            
+        q=collections.deque()
+        if startGene == endGene:
+            return 0
+        q.append((startGene,0))
+        seen=set()
+        seen.add(startGene)
+        while q:
+            s,steps=q.popleft()
+            neigh=neighbors(s)
+            for i in neigh:
+                if i==endGene:
+                    return steps+1
+                if i not in seen and i in bank:
+                    seen.add(i)
+                    q.append((i,steps+1))
+        return -1
