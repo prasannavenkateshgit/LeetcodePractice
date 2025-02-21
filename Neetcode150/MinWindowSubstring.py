b8de69903e6b0d18f5f0490b45d5efcc635a7066
prasannavenkateshgit <prasannav710@gmail.com>
2025-02-20 19:05:06 -0800

    Create MinWindowSubstring.py

---
+++ Neetcode150/MinWindowSubstring.py
@@ -0,0 +1,36 @@
+'''
+Given two strings s and t, return the shortest substring of s such that every character in t, including duplicates, is present in the substring. If such a substring does not exist, return an empty string "".
+
+You may assume that the correct output is always unique.
+'''
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t=="":
+            return ""
+        countT, window = {}, {}
+        for i in t:
+            if i in countT:
+                countT[i]+=1
+            else:
+                countT[i]=1
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l=0
+        for r in range(len(s)):
+            c = s[r]
+            if c in window:
+                window[c]+=1
+            else:
+                window[c]=1
+            if c in countT and window[c]==countT[c]:
+                have+=1
+            while have==need:
+                if (r-l+1)<resLen:
+                    res = [l,r]
+                    resLen = r-l+1
+                window[s[l]]-=1
+                if s[l] in countT and window[s[l]]<countT[s[l]]:
+                    have-=1
+                l+=1
+        l,r=res
+        return s[l:r+1] if resLen != float("infinity") else ""
