6ced6924655517410e9b7a0e533e5111f441a1a2
prasannavenkateshgit <prasannav710@gmail.com>
2025-02-25 19:32:18 -0800

    Create DeleteNodeInABST.py

---
+++ Data Structures/Tree/DeleteNodeInABST.py
@@ -0,0 +1,33 @@
+'''
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        if key>root.val:
+            root.right=self.deleteNode(root.right,key)
+        elif key<root.val:
+            root.left=self.deleteNode(root.left,key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            cur=root.right
+            while cur.left:
+                cur=cur.left
+            root.val=cur.val
+            root.right=self.deleteNode(root.right,root.val)
+        return root
