44488e74b1438cee74b4ffee0ae4940b0d6ac919
prasannavenkateshgit <prasannav710@gmail.com>
2025-03-01 17:51:46 -0800

    Create DestroyingAsteroids.py

---
+++ Data Structures/Greedy/DestroyingAsteroids.py
@@ -0,0 +1,18 @@
+'''
+You are given an integer mass, which represents the original mass of a planet. You are further given an integer array asteroids, where asteroids[i] is the mass of the ith asteroid.
+
+You can arrange for the planet to collide with the asteroids in any arbitrary order. If the mass of the planet is greater than or equal to the mass of the asteroid, the asteroid is destroyed and the planet gains the mass of the asteroid. Otherwise, the planet is destroyed.
+
+Return true if all asteroids can be destroyed. Otherwise, return false.
+'''
+from heapq import *
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        heapify(asteroids)
+        while asteroids:
+            val=heappop(asteroids)
+            if val>mass:
+                return False
+            else:
+                mass+=val
+        return True
