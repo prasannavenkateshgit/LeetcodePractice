9c54c5a44c60d4e43d0853c1565b2917373f7c56
prasannavenkateshgit <prasannav710@gmail.com>
2025-03-01 17:17:08 -0800

    Create KthLargestElementInAStream.py

---
+++ Data Structures/Heaps/KthLargestElementInAStream.py
@@ -0,0 +1,30 @@
+'''
+You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+
+You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+
+Implement the KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+'''
+from heapq import *
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        heapify(nums)
+        self.heap=nums
+        self.k=k
+        
+
+    def add(self, val: int) -> int:
+        heappush(self.heap,val)
+        while len(self.heap)>self.k:
+            heappop(self.heap)
+        return self.heap[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
