80eb96f25fc81013cfb48f06f6a050f6730f52bf
prasannavenkateshgit <prasannav710@gmail.com>
2025-03-20 11:12:55 -0700

    Create LCAOfBinaryTree3.py

---
+++ Data Structures/Tree/LCAOfBinaryTree3.py
@@ -0,0 +1,32 @@
+'''
+Given two nodes of a binary tree p and q, return their lowest common ancestor (LCA).
+
+Each node will have a reference to its parent node. The definition for Node is below:
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
+According to the definition of LCA on Wikipedia: "The lowest common ancestor of two nodes p and q in a tree T is the lowest node that has both p and q as descendants (where we allow a node to be a descendant of itself)."
+'''
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        pc=p
+        qc=q
+        while pc!=qc:
+            qc= qc.parent if qc else p
+            pc = pc.parent if pc else q
+        return pc
+        
